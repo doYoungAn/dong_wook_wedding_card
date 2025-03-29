e840abe387e47468fe9aeeb107cdd301c1660ece
doYoungAn <jispoop@gmail.com>
2025-03-29 11:48:31 +0900

    탑 배너 생성

--- src/components2/topBanner/TopBanner.tsx
+++ src/components2/topBanner/TopBanner.tsx
@@ -1,10 +1,34 @@
 import { FC } from 'react';
+import Love from '../icon/love';
 import Sign from '../icon/sign';
+import Left from '../icon/left';
+import Right from '../icon/right';
+import MainImage from './../../assets/images/main.webp';
 
 const TopBanner: FC = () => {
 
   return (
-    <div className="py-[40px]">
+    <div className="py-[40px] mt-[40px]">
+      <div className=' text-primary2 w-full mb-[50px] flex items-center justify-center text-xl'>
+        <span>DONGWOOK</span>
+        <span className='w-[11px] flex mx-[11px]'>
+          <Love />
+        </span>
+        <span>BOGYEONG</span>
+      </div>
+
+      <div className='px-[16px] w-full mb-[48px] flex items-center justify-start'>
+        <Left />
+        <div className='mx-[1em] w-full max-w-full'>
+          <div className='w-full relative overflow-hidden' style={{ paddingBottom: '100%' }}>
+            <img src={MainImage} alt="" className='absolute top-[-70px] left-0 object-cover' />
+          </div>
+        </div>
+        <Right />
+      </div>
+
+
+
       <p className="w-full text-center font-BodoniModa text-primary2 font-semibold" style={{ fontSize: '1.625em', letterSpacing: '0.5px' }}>
         2025.06.07
       </p>
