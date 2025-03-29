670b6fdb6a6202bfaae92ca19c8bbb06dace9d05
doYoungAn <jispoop@gmail.com>
2025-03-29 12:29:01 +0900

    갤러리 2 -> 3층

--- src/components2/gallery/Gallery.tsx
+++ src/components2/gallery/Gallery.tsx
@@ -15,7 +15,7 @@ const Gallery: FC = () => {
   return (
     <>
       <div className='flex overflow-y-auto gap-[10px] flex-nowrap mb-[10px] px-[10px]'>
-        {Gallery_IMAGES.slice(0, 10).map((image, index) => (
+        {Gallery_IMAGES.slice(0, 6).map((image, index) => (
           <div
             className='min-w-[140px] max-w-[140px] h-[205px]'
             onClick={() => onClickImage(index)}
@@ -24,11 +24,21 @@ const Gallery: FC = () => {
           </div>
         ))}
       </div>
-      <div className='flex overflow-y-auto gap-[10px] flex-nowrap px-[10px]'>
-        {Gallery_IMAGES.slice(10, Gallery_IMAGES.length).map((image, index) => (
+      <div className='flex overflow-y-auto gap-[10px] flex-nowrap mb-[10px] px-[10px]'>
+        {Gallery_IMAGES.slice(6, 14).map((image, index) => (
           <div
             className='min-w-[140px] max-w-[140px] h-[205px]'
-            onClick={() => onClickImage(10 + index)}
+            onClick={() => onClickImage(7 + index)}
+          >
+            <img src={image.src} alt="" className="w-full h-full object-cover" />
+          </div>
+        ))}
+      </div>
+      <div className='flex overflow-y-auto gap-[10px] flex-nowrap px-[10px]'>
+        {Gallery_IMAGES.slice(14, Gallery_IMAGES.length).map((image, index) => (
+          <div
+            className='min-w-[140px] max-w-[140px] h-[205px]'
+            onClick={() => onClickImage(14 + index)}
           >
             <img src={image.src} alt="" className="w-full h-full object-cover" />
           </div>
